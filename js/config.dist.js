4b03e796ac38f5e9359fb29ac13e0cb7bc8c8e51
John Miotchell <neufena@minusmoney.co.uk>
2013-01-31 21:41:22 +0000

    .removed old files

--- js/config.dist.js
+++
@@ -1,6 +0,0 @@
-var databaseName = ''; //com.yourcompany.yourapp.db
-var displayName = ''; //This App Database
-var estimatedSize = 3 * 1024 * 1024; //Database size
-var ajaxHost = ''; //Host for AJAX requests inc trialing slash
-var phoneGap = false //is this PhoneGap or Mobile Web
-var debug = false; //Set this to true to see console debug messages
